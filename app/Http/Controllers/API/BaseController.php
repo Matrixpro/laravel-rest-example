0342f9ac59f394dd1232365f1228f61562639059
Matrixpro <jsw.contact@gmail.com>
2022-02-11 13:27:48 -0700

    Readme and cleanup

--- app/Http/Controllers/API/BaseController.php
+++ app/Http/Controllers/API/BaseController.php
@@ -1,13 +1,21 @@
 <?php
 
-
 namespace App\Http\Controllers\API;
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller as Controller;
 
-
 class BaseController extends Controller
 {
+	/**
+	 * Handle basic API responses
+	 *
+	 * @param      $result  The result
+	 * @param      $msg     The message
+	 * @param      $cursor  The cursor
+	 *
+	 * @return     <JSON>
+	 */
 	public function handleResponse($result, $msg, $cursor=NULL)
 	{
 		$res = [
@@ -22,6 +30,15 @@ class BaseController extends Controller
 		return response()->json($res, 200);
 	}
 
+	/**
+	 * Handle basic API error responses
+	 *
+	 * @param      <string>  $error           The error
+	 * @param      array   $error_messages  The error messages
+	 * @param      int     $code            The code
+	 *
+	 * @return     <JSON>
+	 */
 	public function handleError($error, $error_messages = [], $code = 404)
 	{
 	
